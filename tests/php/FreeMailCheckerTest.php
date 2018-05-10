c41b83cace49a6f9b320aefeb9f8b72191ec4537
salaros <salaros@yandex.ru>
2018-05-10 22:50:21 +0300

    adding PHP implementation

---
+++ tests/php/FreeMailCheckerTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Test case for FreeMailChecker class
+ *
+ * @category PHPUnit
+ * @package  FreeMailChecker
+ * @author   Zhmayev Yaroslav <salaros@salaros.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/salaros/free-mailchecker
+ */
+
+declare(strict_types=1);
+
+namespace Salaros\Email;
+
+use Salaros\Email\FreeMailChecker;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test case for FreeMailChecker class
+ */
+final class FreeMailCheckerTest extends TestCase
+{
+    /**
+     * Check if any free domain is defined
+     *
+     * @return void
+     */
+    public function testCanBeCreatedFromValidEmailAddress()
+    {
+        $obj = new FreeMailChecker();
+        $this->assertNotEmpty($obj->getFreeDomains());
+    }
+
+    /**
+     * Tests FreeMailChecker->isFreeMailAddress() against some free emails
+     *
+     * @param string $freeEmail A free email address
+     *
+     * @dataProvider getFreeMailDomainSamples
+     *
+     * @return void
+     */
+    public function testEmailAddressIsOnFreeDomain($freeEmail)
+    {
+        $obj = new FreeMailChecker();
+        $this->assertSame(true, $obj->isFreeMailAddress($freeEmail));
+    }
+
+    /**
+     * Tests FreeMailChecker->isFreeMailAddress() against some business emails
+     *
+     * @param string $businessEmail A business email address
+     *
+     * @dataProvider getBusinessMailDomainSamples
+     *
+     * @return void
+     */
+    public function testEmailAddressIsOnBusinessDomain($businessEmail)
+    {
+        $obj = new FreeMailChecker();
+        $this->assertSame(false, $obj->isFreeMailAddress($businessEmail));
+    }
+
+    /**
+     * Tests FreeMailChecker->isValid() against some business emails
+     *
+     * @param string $businessEmail A business email address
+     *
+     * @depends      testEmailAddressIsOnBusinessDomain
+     * @dataProvider getBusinessMailDomainSamples
+     *
+     * @return void
+     */
+    public function testEmailAddressIsValid($businessEmail)
+    {
+        $obj = new FreeMailChecker();
+        $this->assertSame(true, $obj->isValid($businessEmail));
+    }
+
+    /**
+     * Provides a list of free email addresses
+     *
+     * @codeCoverageIgnore
+     *
+     * @return array
+     */
+    public function getFreeMailDomainSamples()
+    {
+        return [
+            array('free_@gmail.com'),
+            array('free_@yahoo.it'),
+            array('free_@yandex.ru'),
+        ];
+    }
+
+    /**
+     * Provides a list of business email addresses
+     *
+     * @codeCoverageIgnore
+     *
+     * @return array
+     */
+    public function getBusinessMailDomainSamples()
+    {
+        return array(
+            array('biz_@microsoft.com'),
+            array('biz_@apple.com'),
+            array('biz_@mysql.org'),
+        );
+    }
+}
