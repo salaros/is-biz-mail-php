6f6d8b9db0180a15db54fe7200ed642e52a0e41b
Yaroslav Zhmayev <salaros@yandex.ru>
2021-02-25 15:37:16 +0300

    fixing parse error on PHP 5.4 + trying to make 8.0 version work too

--- tests/IsBizMailTest.php
+++ tests/IsBizMailTest.php
@@ -156,7 +156,7 @@ final class IsBizMailTest extends TestCase
     public function testisFreeMailAddressException200()
     {
         if (version_compare(PHP_VERSION, '7.3.0') >= 0) {
-            $this->expectException(\InvalidArgumentException::class);
+            eval('$this->expectException(\InvalidArgumentException::class);'); // needed to avoid parse error on PHP 5.4
             $this->expectExceptionCode(200);
         }
 
@@ -176,10 +176,10 @@ final class IsBizMailTest extends TestCase
     public function testisFreeMailAddressException100()
     {
         if (version_compare(PHP_VERSION, '7.3.0') >= 0) {
-            $this->expectException(\InvalidArgumentException::class);
+            eval('$this->expectException(\InvalidArgumentException::class);'); // needed to avoid parse error on PHP 5.4
             $this->expectExceptionCode(100);
         }
-        
+
         (new IsBizMail())->isFreeMailAddress('notAnEmail');
     }
 
